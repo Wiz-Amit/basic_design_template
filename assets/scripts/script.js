0002384fb18ae81bcef2e72b043efbbaa6b9272d
Wiz-Amit <ahazratwt@gmail.com>
2020-05-11 23:28:15 +0530

    boilerplate code

---
+++ assets/scripts/script.js
@@ -0,0 +1,14 @@
+//@ts-check
+"use strict";
+
+window.addEventListener("load", init);
+
+function init() {
+  // initialize all functions
+  hello();
+}
+
+// :: FUNCTIONS ::
+function hello() {
+  console.log("Script has been loaded successfully!");
+}
